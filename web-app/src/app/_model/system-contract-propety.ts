2cd1f676919a9a2efee38db1f8ef54c24524ad7e
Ovcharov Michail <ombp@mail.ru>
2021-05-16 22:36:23 +0500

    Изменения

---
+++ web-app/src/app/_model/system-contract-propety.ts
@@ -0,0 +1,13 @@
+export class SystemContractProperty {
+  id: number;
+  value: string;
+  beginDate: string;
+  endDate: string;
+  property_type_id: number;
+  contractor_id: number;
+  inputType: string;
+  createBy: string;
+  createDate: string;
+  lastModifiedBy: string;
+  lastModifiedByDate: string;
+}
